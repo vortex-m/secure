04c32d46ddc3c3171bb63d0e67b2ccb6748897ec
Mayank <134766594+vortex-m@users.noreply.github.com>
2024-11-11 01:05:35 +0530

    Update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/secure/',  // Replace 'your-repository-name' with the name of your GitHub repository
 })
